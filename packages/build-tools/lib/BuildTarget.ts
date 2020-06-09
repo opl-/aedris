opl-/aedris

commit 9788edc2f0f703ff9fb060364ecd1dcf41b96308

Move global instance of RuntimePluginLoader out of a virtual module
This allows easily requiring it without the need to generate and somehow make the tooling find the declaration files for virtual module.

While I considered moving the runtime out into its own package, in the process I realized that it'd require all packages that want to reference the runtime to declare the runtime package as a dependency, which would add unnecessary complexity to the dependencies. For that reason it's staying as part of the build tools which every package will almost certainly already depend on. Creating a webpack alias for the runtime instance in the future is an option that might be worth looking into, though it would reduce transparency a little.

packages/build-tools/lib/runtime/index.ts
@@ -1,0 +1,9 @@
+import RuntimePluginLoader from './RuntimePluginLoader';
+
+// Export a global RuntimePluginLoader instance for the whole app
+const loaderInstance = new RuntimePluginLoader();
+export default loaderInstance;
+
+export {
+	RuntimePluginLoader,
+};

packages/build-tools/lib/runtime/entryTemplate.ts
@@ -1,7 +1,6 @@
 export default function entryTemplate(plugins: string[]): string {
-	return `import RuntimePluginLoader from '@aedris/build-tools/dist/runtime/RuntimePluginLoader';
+	return `import loader from '@aedris/build-tools/dist/runtime/index';
 
-const loader = new RuntimePluginLoader();
 [
 	${plugins.map((m) => `[${JSON.stringify(m)}, import(${JSON.stringify(m)})]`).join(', ')}
 ].forEach((plugin) => loader.registerPlugin(plugin[0], plugin[1]));

commit 5ce7304772289aec7be340f230bc81c7e5d6029e

Use a hook for determining BuildTarget's externals
Fixes a regression introduced in 9788edc causing the frontend Vue bundles used for SSR to interact with the backend Koa bundle, resulting in the backend being started multiple times and causing all sorts of weirdness and headaches.

Externals uses a hook instead of webpack-chain like everything else because webpack-chain still doesn't support externals and I'm not sure how to implement it without breaking backwards compatibility horribly. See neutrinojs/webpack-chain#222.

This is the first commit in what will hopefully soon become a series of commits for all the stuff I've done in the past week or so.

packages/build-tools/lib/BuildTarget.ts
@@ -1,4 +1,5 @@
-import {Configuration, ExternalsElement, Compiler} from 'webpack';
+import {AsyncSeriesBailHook} from 'tapable';
+import {Configuration, Compiler} from 'webpack';
 import ChainConfig from 'webpack-chain';
 import VirtualModulesPlugin from 'webpack-virtual-modules';
 
@@ -24,7 +25,17 @@ export interface TargetOptions {
 	outputDir: string;
 }
 
+export interface ExternalsQuery {
+	context: string;
+	request: string;
+}
+
 export class BuildTarget {
+	hooks = {
+		/** Used as a replacement for the webpack externals config option as webpack-chain doesn't support it. See https://github.com/neutrinojs/webpack-chain/issues/222. */
+		externalsQuery: new AsyncSeriesBailHook<ExternalsQuery, undefined, undefined, string | false | undefined>(['query']),
+	};
+
 	name: string;
 
 	builder: Builder;
@@ -41,12 +52,6 @@ export class BuildTarget {
 
 	/** The entry points for this target, including the generated app entry point if applicable */
 	entry: {[entryName: string]: string[]};
-
-	/**
-	 * webpack externals to be used in this target. The key is used only for referencing the externals definition from other plugins and is not used in the final config. This property exists because
-	 * webpack-chain doesn't handle externals.
-	 * */
-	externals: {[externalsName: string]: ExternalsElement};
 
 	/** The output directory for this target, relative to the output directory specified in the config */
 	outputDir: string;
@@ -90,9 +95,6 @@ export class BuildTarget {
 		this.virtualModules = {};
 		this.virtualModulesPlugin = undefined;
 
-		// Reset externals object
-		this.externals = {};
-
 		if (!this.config.isPlugin) {
 			// Compute entry points to ensure they include the generated entry point for apps
 			this.entry = Object.entries(this.rawEntry).reduce((acc, [entryName, entryPluginNames]) => {
@@ -121,11 +123,17 @@ export class BuildTarget {
 		// Allow plugins to extend the generated config
 		configChain = this.builder.hooks.prepareWebpackConfig.call(configChain, this);
 
-		if (configChain.has('externals')) throw new Error('Use BuildTarget.externals for externals to allow manipulating them from other plugins');
+		if (configChain.has('externals')) throw new Error('Use BuildTarget.hooks.externalsQuery for externals to allow manipulating them from other plugins');
 
 		// Construct externals
-		const externalsArray = Object.values(this.externals);
-		configChain.set('externals', externalsArray);
+		configChain.set('externals', (context: string, request: string, callback: (err: any, result?: string | false) => void) => {
+			this.hooks.externalsQuery.promise({
+				context,
+				request,
+			}).then((result) => {
+				callback(undefined, result);
+			}).catch(callback);
+		});
 
 		// Finalize config
 		this.webpackConfig = configChain.toConfig();

packages/build-tools/lib/runtime/index.ts
@@ -1,3 +1,10 @@
+/**
+ * This file is special. It is never externalized when building the app to prevent the app's global loader instance from leaking into other apps.
+ * Unfortunately, due to the current implementation of the `util/externals` it is impossible to not externalize this module while externalizing all of its dependencies, so any new imports in this
+ * file need to be added to the `webpack.base.ts`.
+ */
+// FIXME: remove above comment after `util/externals` is fixed
+
 import RuntimePluginLoader from './RuntimePluginLoader';
 
 // Export a global RuntimePluginLoader instance for the whole app

packages/build-tools/lib/util/externals.ts
@@ -1,12 +1,18 @@
+import {ExternalsQuery} from '../BuildTarget';
+
 const moduleNameTest = /^(?:@[a-z0-9\-_]+?\/)?[a-z0-9\-_]+(?=$|\/)/;
 
 // TODO: this might include files included from externals using relative paths
 
 export default function externalsGenerator({whitelist} = {whitelist: /./}) {
-	return function externals(context: string, request: string, callback: (err: any, result: string | undefined) => void): void {
+	return function externals(query: ExternalsQuery): string | undefined {
 		// Ignore @aedris/entry and @aedris/dynamic imports: those are special and handled by other parts of the code
-		if (/^@aedris\/(?:entry|dynamic)(?:\/.+)?$/.test(request)) return void callback(undefined, undefined);
+		if (/^@aedris\/(?:entry|dynamic)(?:\/.+)?$/.test(query.request)) return undefined;
 
-		callback(undefined, moduleNameTest.test(request) && !whitelist.test(request) ? `commonjs ${request}` : undefined);
+		// If the request is for any external module and isn't whitelisted, externalize it
+		// eslint-disable-next-line consistent-return
+		if (moduleNameTest.test(query.request) && !whitelist.test(query.request)) return `commonjs ${query.request}`;
+
+		return undefined;
 	};
 }

packages/build-tools/lib/webpack-config/webpack.base.ts
@@ -10,7 +10,7 @@ import externalsGenerator from '../util/externals';
 // TODO: add aliases
 
 export default <WebpackConfigCreator> function createWebpackConfig(config, target) {
-	const {builder, config: aedrisConfig, externals} = target;
+	const {builder, config: aedrisConfig} = target;
 
 	config.mode(builder.isDevelopment ? 'development' : 'production');
 
@@ -57,21 +57,49 @@ export default <WebpackConfigCreator> function createWebpackConfig(config, targe
 	config.resolve.modules.merge(modulePaths);
 
 	// Replace any-promise with native Promise object. See https://github.com/kevinbeaty/any-promise/issues/28
-	externals['any-promise'] = {'any-promise': 'Promise'};
+	target.hooks.externalsQuery.tap('@aedris/build-tools:any-promise', (context) => {
+		if (context.request === 'any-promise') return 'Promise';
+		// eslint-disable-next-line consistent-return
+		return undefined;
+	});
 
 	// Don't include node dependencies in a node context
 	if (target.context.includes(DefaultContext.NODE)) {
-		externals['node-externals'] = externalsGenerator({
+		target.hooks.externalsQuery.tap({
+			name: '@aedris/build-tools:node-externals',
+			// As node externals are a very broad category, run them late
+			stage: 10000,
+		}, externalsGenerator({
 			// With the exception of files that need to be processed by webpack
 			whitelist: /^\.(css|less|s[ac]ss|styl)$/,
-		});
+		}));
 	}
 
 	// Resolve dynamic modules only when building an app
 	if (aedrisConfig.isPlugin) {
-		externals['dynamic-modules'] = function dynamicModules(context: string, request: string, callback: (err?: Error, result?: string) => void) {
-			callback(undefined, /^@aedris\/dynamic\/.+$/.test(request) ? `commonjs ${request}` : undefined);
-		};
+		// eslint-disable-next-line consistent-return
+		target.hooks.externalsQuery.tap('@aedris/build-tools:dynamic-modules', (query) => (/^@aedris\/dynamic\/.+$/.test(query.request) ? `commonjs ${query.request}` : undefined));
+	}
+
+	// Externalize the runtime index in order to prevent the global RuntimePluginLoader instance from leaking into other app packages when multiple run in the same node context.
+	// We should only externalize modules when running in a node context.
+	if (!aedrisConfig.isPlugin && target.context.includes(DefaultContext.NODE)) {
+		target.hooks.externalsQuery.tap({
+			name: '@aedris/build-tools:bundleSpecificRuntimePluginLoader',
+			// Run this hook early to reduce the chances of other hooks catching the module before us
+			stage: -1000,
+		}, (query) => {
+			// Force all modules required from the runtime index to be externalized when they should.
+			// Yes, the imports to externalize are listed here manually and need to be updated if any new imports are added. This shouldn't be an issue once externalsGenerator is fixed.
+			if (query.request === './RuntimePluginLoader' && query.context.endsWith('build-tools/dist/runtime')) {
+				return 'commonjs @aedris/build-tools/dist/runtime/RuntimePluginLoader';
+			}
+
+			if (/^@aedris\/build-tools\/dist\/runtime(?:\/index)?$/.test(query.request)) return false;
+
+			// eslint-disable-next-line consistent-return
+			return undefined;
+		});
 	}
 
 	// Rules for TypeScript files

packages/framework-koa/lib/build.ts
@@ -97,15 +97,12 @@ export default <AedrisPlugin> {
 		});
 
 		builder.hooks.prepareWebpackConfig.tap(HOOK_NAME, (config, target) => {
-			// Override the build-tools externals function to never externalize our own entry bundles for dynamic module resolution. Kinda hacky but gets the job done.
-			const originalExternals = target.externals['node-externals'];
-			if (originalExternals) {
-				// eslint-disable-next-line no-param-reassign
-				target.externals['node-externals'] = (context: string, request: string, callback: (err?: Error, result?: string) => void) => {
-					if (/@aedris\/framework-koa\/dist\/backend/.test(request)) return callback(undefined, undefined);
-					return (originalExternals as Function)(context, request, callback);
-				};
-			}
+			// Never externalize our own entry bundles when building the app for dynamic module resolution used in those bundles to work
+			target.hooks.externalsQuery.tap(HOOK_NAME, (query) => {
+				if (query.request === '@aedris/framework-koa/dist/backend') return false;
+				// eslint-disable-next-line consistent-return
+				return undefined;
+			});
 
 			return config;
 		});

packages/framework-vue/lib/build.ts
@@ -113,15 +113,12 @@ export default <AedrisPlugin> {
 		});
 
 		builder.hooks.prepareWebpackConfig.tap(HOOK_NAME, (config, target) => {
-			// Override the build-tools externals function to never externalize our own entry bundles for dynamic module resolution. Kinda hacky but gets the job done.
-			const originalExternals = target.externals['node-externals'];
-			if (originalExternals) {
-				// eslint-disable-next-line no-param-reassign
-				target.externals['node-externals'] = (context: string, request: string, callback: (err?: Error, result?: string) => void) => {
-					if (/@aedris\/framework-vue\/dist\/(?:entryFrontend(?:Client|Server))/.test(request)) return callback(undefined, undefined);
-					return (originalExternals as Function)(context, request, callback);
-				};
-			}
+			// Never externalize our own entry bundles when building the app for dynamic module resolution used in those bundles to work
+			target.hooks.externalsQuery.tap(HOOK_NAME, (query) => {
+				if (/^@aedris\/framework-vue\/dist\/(?:entryFrontend(?:Client|Server)$)/.test(query.request)) return false;
+				// eslint-disable-next-line consistent-return
+				return undefined;
+			});
 
 			return config;
 		});