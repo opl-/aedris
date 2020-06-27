e40fb5b600a81f978aebde08b9f9f0151c40f076
opl- <opl-@users.noreply.github.com>
2020-06-27 07:44:59 +0200

    framework-koa: Add docs for expressMiddlewareWrapper

--- packages/framework-koa/backend/util/expressMiddlewareWrapper.ts
+++ packages/framework-koa/backend/util/expressMiddlewareWrapper.ts
@@ -1,6 +1,12 @@
 import {IncomingMessage, ServerResponse} from 'http';
 import {Middleware} from 'koa';
 
+/**
+ * A small wrapper that allows using middleware designed for Express with Koa. Note that it does not create any shims for the `req, res` objects, and as such will not work with all middleware.
+ *
+ * @param middleware The Express middleware we want to use under Koa
+ * @returns Function with Koa's middleware signature
+ */
 export default function expressMiddlewareWrapperFactory(middleware: (req: IncomingMessage, res: ServerResponse, next: (err?: any) => void) => any): Middleware {
 	return (ctx, next) => new Promise((resolve, reject) => {
 		try {
